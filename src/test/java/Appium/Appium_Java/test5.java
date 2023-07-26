62524bf1a825933b1e8ec66de1f2bf2973fc2dc6
amar mali <amardmali11@gmail.com>
2023-07-26 09:21:36 +0530

    new code changes in driver file

--- src/test/java/Appium/Appium_Java/test5.java
+++ src/test/java/Appium/Appium_Java/test5.java
@@ -10,9 +10,10 @@ public class test5 {
 	public void test5()
 	{
 		System.out.println("print test 5");
-		System.setProperty("webdriver.chrome.driver", "G:\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "G:\\API Automation Udemy Class\\Appium_Java\\src\\main\\java\\utils\\driver\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://www.udemy.com");
 		driver.manage().window().maximize();
+		driver.close();
 	}
 }
